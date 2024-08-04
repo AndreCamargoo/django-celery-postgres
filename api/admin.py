45169e0b15f037534ddea6b1d664d7f2915f7973
André Camargo <andre.camargo@msn.com>
2024-08-04 16:47:42 -0300

    Registrando admin.py

--- api/admin.py
+++ api/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+from django.contrib import admin
+from .models import UserData
+
+
+admin.site.register(UserData)
